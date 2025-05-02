51957c5cec668762017f279e6a39d35e84810ca5
mo-amaan <mo.amaan215@gmail.com>
2025-05-02 15:55:57 -0300

    Initial changes

---
+++ app/error.js
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function Error(props) {
+  const { error, reset } = props;
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div>
+      <h3>Something Went Wrong 😅</h3>
+      <div>
+        <button onClick={reset}>reset</button>
+        <Link href={"/"}>
+          <button>Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ components/ImageBanner.jsx
@@ -0,0 +1,6 @@
+// Handle the smooth loading from the low-res to med-res
+export default function ImageBanner(){
+  return(
+    <div>ImageBanner</div>
+  )
+}
